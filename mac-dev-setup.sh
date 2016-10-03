0203167a132e3ce97efa96c7f055f634255554ec
Bukhori Muhammad Aqid <aa.qidd@gmail.com>
2016-10-03 18:05:30 +0700

    update install script
    add java & java home path, add android home path, add cordova installation (optional).

--- mac-dev-setup.sh
+++ mac-dev-setup.sh
@@ -76,6 +76,7 @@ apps=(
 	flux
 	google-chrome
 	imageoptim
+	java
 	onyx
 	postman
 	qlcolorcode
@@ -104,6 +105,14 @@ brew cask install --appdir="/Applications" ${apps[@]}
 
 #install android-sdk
 brew install android-sdk
+
+#add JAVA_HOME & ANDROID_HOME
+echo '# Create a JAVA_HOME variable, determined dynamically' >> ~/.bash_profile
+echo 'export JAVA_HOME=$(/usr/libexec/java_home)' >> ~/.bash_profile
+echo '# Add that to the global PATH variable' >> ~/.bash_profile
+echo 'export PATH=${JAVA_HOME}/bin:$PATH' >> ~/.bash_profile
+echo '# Add the Android SDK to the PATH variable' >> ~/.bash_profile
+echo 'export PATH=${PATH}:~/Library/Android/sdk/tools:~/Library/Android/sdk/platform-tools' >> ~/.bash_profile
 
 #install mac app store command line
 brew install mas
@@ -135,6 +144,12 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 
 #restart dock
 killall Dock
+
+#extras: ionic & cordova for hybrid mobile app development
+# sudo npm install -g cordova
+# sudo npm install -g ionic
+# sudo npm install -g ios-sim
+# sudo npm install -g ios-deploy --unsafe-perm=true
 
 echo "cleanup"
 brew cleanup
